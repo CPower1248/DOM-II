73eea24dbdac891edcd9acc4fc53a517217c858e
corey-power <CPower1248@gmail.com>
2020-09-29 17:06:49 -0400

    Added 5 event listeners, stopPropagation and preventDefault

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+document.querySelector('a').addEventListener('click', (event) => {
+    event.preventDefault()
+  })
+// mouseover .logo-heading
+document.querySelector('.logo-heading').addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+    event.stopPropagation()
+})
+document.querySelector('.logo-heading').addEventListener('mouseout', (event) => {
+    event.target.style.color = 'black';
+    event.stopPropagation()
+})
+// onclick .nav-link
+const newNav = document.querySelectorAll(".nav-link")
+newNav.forEach((item) => {
+    item.addEventListener("click", (event) => {
+        event.target.style.color = "blue";
+    })
+})
+// load .intro
+document.querySelector(".intro h2").addEventListener("dblclick", (event) => {
+    event.target.style.fontSize = "5rem";
+})
+// keydown .intro p
+document.querySelector(".intro p").addEventListener("wheel", (event) => {
+    event.target.style.color = "green";
+})
