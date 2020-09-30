ad799ea694a6b9f9e6938bafbe0bfd20d4030b00
corey-power <CPower1248@gmail.com>
2020-09-29 21:04:35 -0400

    Met MVP

--- js/index.js
+++ js/index.js
@@ -22,7 +22,30 @@ newNav.forEach((item) => {
 document.querySelector(".intro h2").addEventListener("dblclick", (event) => {
     event.target.style.fontSize = "5rem";
 })
-// keydown .intro p
+// wheel .intro p
 document.querySelector(".intro p").addEventListener("wheel", (event) => {
-    event.target.style.color = "green";
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+})
+// keydown .content-section .text-content h2
+const newLetsGo = document.querySelector(".content-section .text-content h2")
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'e') {
+        // This changes the text design when pressing the event key 'e'
+        newLetsGo.textContent = 'You pressed "e" so this changed!!!';
+        newLetsGo.style.color = 'red';
+        newLetsGo.style.fontSize = '6rem';
+        newLetsGo.style.textTransform = 'uppercase';
+    }
+})
+// load window
+window.addEventListener('load', (event) => {
+    console.log("Hello World!")
+});
+//auxclick img
+const newImg = document.querySelectorAll("img")
+newImg.forEach((item) => {
+    item.addEventListener("auxclick", (event) => {
+        event.target.style.display = "none";
+    })
 })
